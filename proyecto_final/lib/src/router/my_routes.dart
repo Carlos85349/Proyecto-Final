9094dda456f668ae97593a2a0cd2bebae6ef988a
Carlos85349 <147650880+Carlos85349@users.noreply.github.com>
2023-12-07 12:06:37 -0600

    Cambios

--- proyecto_final/lib/src/router/my_routes.dart
+++ proyecto_final/lib/src/router/my_routes.dart
@@ -1,12 +1,15 @@
-
 import 'package:proyecto_final/src/pages/login_page.dart';
+import 'package:proyecto_final/src/pages/peliculas_page.dart';
+import 'package:proyecto_final/src/pages/pelicularating_page.dart';
 import 'package:flutter/material.dart';
 
 //enumarate
-enum Routes { home, login, images, listas, peticiones }
+enum Routes { login, peliculas, rating }
 
 class MyRoutes {
   static final routes = {
     Routes.login.name: (BuildContext context) => LoginPage(),
+    Routes.peliculas.name: (BuildContext context) => LoginPage(),
+    Routes.rating.name: (BuildContext context) => LoginPage(),
   };
 }
